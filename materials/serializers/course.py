9a3606c92f63e1e8b9f3db58528c4d3582eb7b23
Inna Antypova <nightallatiel@gmail.com>
2024-02-15 23:03:55 +0300

    добавила счетчик уроков в курсе

--- materials/serializers/course.py
+++ materials/serializers/course.py
@@ -4,6 +4,12 @@ from materials.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()  # счетчик уроков в курсе
+
+    def get_lessons_count(self, instance):
+        """ Метод для подсчета кол-ва уроков в курсе."""
+        return instance.name_course.all().count()
+
     class Meta:
         model = Course
         fields = '__all__'
