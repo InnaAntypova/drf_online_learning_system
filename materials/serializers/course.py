39935ffd510d9eb7becb78660327fd9fcc56bdad
Inna Antypova <nightallatiel@gmail.com>
2024-03-03 19:46:18 +0300

    добавила отображение подписки в курсе

--- materials/serializers/course.py
+++ materials/serializers/course.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.serializers.lesson import LessonSerializer
 from materials.validators import URLValidator
 
@@ -8,6 +8,7 @@ from materials.validators import URLValidator
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()  # счетчик уроков в курсе
     lessons_in_course = serializers.SerializerMethodField()  # уроки в курсе
+    subscription = serializers.SerializerMethodField()  # подписка на курс
 
     def get_lessons_count(self, instance):
         """ Метод для подсчета кол-ва уроков в курсе."""
@@ -16,6 +17,12 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_lessons_in_course(self, instance):
         """ Метод для вывода уроков курса."""
         return LessonSerializer(Lesson.objects.filter(course=instance), many=True).data
+
+    def get_subscription(self, instance):
+        """ Метод для получения подписки на курс (True/False)"""
+        request = self.context.get('request')
+        # print(request.user)
+        return Subscription.objects.filter(course=instance, user=request.user).exists()
 
     class Meta:
         model = Course
